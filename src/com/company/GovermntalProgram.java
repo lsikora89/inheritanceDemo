7aec9b207575d380474dd12147e60df15c39ce68
lsikora89 <lsikora@student.bridgew.edu>
2019-10-03 12:12:37 -0400

    10/3/19

--- src/com/company/GovermntalProgram.java
+++ src/com/company/GovermntalProgram.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public class GovermntalProgram {
+
+    public void processPaperwork(double charge){
+        System.out.println("Gov is Processing a charge of" +charge);
+    }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Person {
+
+    private String name;
+
+    public Person(String give_name){
+        this.name = give_name;
+    }
+
+    public void takeMedicine(){
+        System.out.println("Yuck that medicine taste bad");
+    }
+
+    public void get_operated_on(){
+        System.out.println("Your being cut open");
+    }
 }
